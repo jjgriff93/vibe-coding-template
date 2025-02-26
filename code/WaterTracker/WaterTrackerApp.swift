705478567faf9c00479917b9e7ff053fdca992ca
Pierce Boggan <piboggan@microsoft.com>
2025-02-26 02:25:36 -0700

    Initialize WaterTracker app with main structure and basic UI components

---
+++ code/WaterTracker/WaterTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackerApp.swift
+//  WaterTracker
+//
+//  Created by Pierce Boggan on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct WaterTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
